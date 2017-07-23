6f26090ef06cc9bb82bc54f328ce0ecdc309456d
e <494518881@qq.com>
2017-07-23 20:39:05 +0800

    ass

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+=======
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+>>>>>>> second change
